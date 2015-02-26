b3b87736b31b879abbcb2848d13e69b9cc6ef7a8
Leon <seamyself@gmail.com>
2015-02-26 17:41:08 -0600

    add PushForce() in Queue. This will force to override the oldest element if the queue is full. Check TestFunciton3() for usage.

--- apps/test_queue/wixel_queue.c
+++ apps/test_queue/wixel_queue.c
@@ -56,6 +56,7 @@ typedef struct Queue
   // member function declariation
   int (*Push)(Tuple* p);
   int (*Pop)(Tuple* p);
+  int (*PushForce)(Tuple* p);
 } Queue;
 
 //
@@ -81,6 +82,38 @@ int Push(Tuple* p)
   }
   return 0;
 }
+
+//
+// Force push date into the queue.
+// i.e. if the queue is full, then the new pushed elelment will overide oldest element.
+//
+int PushForce(Tuple* p) 
+{
+  int i = 0;
+  int next = 0;
+  Queue* queue = (0==p ? 0: p->caller);
+  if (0!=queue) {
+    next = (queue->tail+1)%ContSize;
+    if (next != queue->head) {
+      // copy p to queue
+      for (i = 0; i < TupleSize; ++i) {
+        queue->data[next].bytes[i] = p->bytes[i];
+      } 
+      queue->tail = next;
+      return 1;
+    } else {
+      for (i = 0; i < TupleSize; ++i) {
+        queue->data[next].bytes[i] = p->bytes[i];
+      } 
+      queue->tail = next;
+      queue->head = (queue->head+1)%ContSize;;
+      return 1;
+    }
+  }
+  return 0;
+}
+
+
 
 //
 // member funciton: Pop
@@ -113,6 +146,7 @@ int InitQueue(Queue* queue)
     queue->head = 0;
     queue->tail = 0;
     queue->Push = Push;
+    queue->PushForce = PushForce;
     queue->Pop = Pop;
     return 1;
   }
@@ -262,17 +296,107 @@ int TestFunction2()
   return 0;
 }
 
+
+//
+// Test ForcePush
+//
+int TestFunction3()
+{
+  // the queue is a container to store Tuple data
+  // you can redefine the TupleSize to change the max size that a Tuple can store. 
+  // here, for example the size is 5.
+  XDATA Queue queue1;
+  // we will push two fivebytes to queue
+  Tuple fivebytes1;
+  // then pop them out to pop_result 
+  Tuple pop_result1;
+
+  int i = 0;
+
+  //*****************************************************************
+  //*** Remember to call init and link fuction before using Queue ***
+  //***  we have to link Queue and Tuple to use member function.  ***
+  //*****************************************************************
+  InitQueue(&queue1);
+  LinkQueueTuple(&queue1, &fivebytes1);
+  LinkQueueTuple(&queue1, &pop_result1);
+
+  // test: push "hello" which is five byte
+  fivebytes1.bytes[0] = 'h';
+  fivebytes1.bytes[1] = 'e';
+  fivebytes1.bytes[2] = 'l';
+  fivebytes1.bytes[3] = 'l';
+  fivebytes1.bytes[4] = 'o';
+  queue1.Push(&fivebytes1);
+
+  // test: push "world" which is five byte
+  fivebytes1.bytes[0] = 'w';
+  fivebytes1.bytes[1] = 'o';
+  fivebytes1.bytes[2] = 'r';
+  fivebytes1.bytes[3] = 'l';
+  fivebytes1.bytes[4] = 'd';
+  queue1.Push(&fivebytes1);
+
+  // test: push 
+  fivebytes1.bytes[0] = 't';
+  fivebytes1.bytes[1] = 'e';
+  fivebytes1.bytes[2] = 's';
+  fivebytes1.bytes[3] = 't';
+  fivebytes1.bytes[4] = '0';
+  queue1.PushForce(&fivebytes1);
+
+  // test: push 
+  fivebytes1.bytes[0] = 't';
+  fivebytes1.bytes[1] = 'e';
+  fivebytes1.bytes[2] = 's';
+  fivebytes1.bytes[3] = 't';
+  fivebytes1.bytes[4] = '1';
+  queue1.PushForce(&fivebytes1);
+
+  // test: push 
+  fivebytes1.bytes[0] = 't';
+  fivebytes1.bytes[1] = 'e';
+  fivebytes1.bytes[2] = 's';
+  fivebytes1.bytes[3] = 't';
+  fivebytes1.bytes[4] = '2';
+  queue1.PushForce(&fivebytes1);
+
+  // test: push 
+  fivebytes1.bytes[0] = 't';
+  fivebytes1.bytes[1] = 'e';
+  fivebytes1.bytes[2] = 's';
+  fivebytes1.bytes[3] = 't';
+  fivebytes1.bytes[4] = '3';
+  queue1.PushForce(&fivebytes1);
+
+
+  // test: queue1 pop out all data
+  while(queue1.Pop(&pop_result1)) {
+    for (i = 0; i < TupleSize; ++i) {
+      MY_PRINT2("%c", pop_result1.bytes[i]);
+    }
+    MY_PRINT("\n");
+  }
+
+  return 0;
+}
+
 int main()
 {
   // The test function shows how to use Queue
 
   // Test one local queue
-  MY_PRINT("Test1:\n");
+  MY_PRINT("\nTest1:\n");
   TestFunction1();
+
   // Test two local queues
-  MY_PRINT("Test2:\n");
+  MY_PRINT("\nTest2:\n");
   TestFunction2();
+
+  // Test Force Push data into queue
+  // to see the effect, redefine the ContSize to smaller size, e.g. 4
+  MY_PRINT("\nTest3:\n");
+  TestFunction3();
   return 0;
 }
-
 
